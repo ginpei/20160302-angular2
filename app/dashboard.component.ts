648af4e9ffc8e2eb427b6fc3629f65105bba75ff
ginpei <ginpei@gmail.com>
2016-03-03 11:31:58 -0800

    dashboard

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,8 +1,21 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
+
+import {Hero} from './hero.ts';
+import {HeroService} from './hero.service';
 
 @Component({
 	selector: 'my-dashboard',
-	template: '<h2>Dashboard</h2>'
+	templateUrl: 'app/dashboard.component.html',
+	styleUrls: ['app/dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+	heroes: Hero[];
+
+	constructor(private _heroService: HeroService) {
+	}
+
+	ngOnInit() {
+		this._heroService.getHeroes()
+			.then(heroes=>this.heroes=heroes.slice(1,5));
+	}
 }
